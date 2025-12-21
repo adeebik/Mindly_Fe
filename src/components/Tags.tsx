3218f0dfea673433f67971b78373121f3b5bd33d
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-22 02:22:42 +0530

    tags improv

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,11 +1,12 @@
 interface TagProps {
     title: string
+    onClick?: ()=> void;
 }
 
 export default function Tags(tags : TagProps) {
   return (
     <>
-        <button className="p-1 border border-blue-300 text-xs font-normal text-blue-600 rounded-md bg-blue-50"> {tags.title}</button>
+        <button onClick={tags.onClick} className="p-1 border border-blue-300 text-xs font-normal text-blue-600 rounded-md bg-blue-50"> {tags.title}</button>
     </>
   )
 }
