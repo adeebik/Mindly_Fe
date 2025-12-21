fe7a592a7e74be441ae557965c738545d9c10446
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-21 22:25:12 +0530

    making frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Plus } from 'lucide-react'
+import Button from './components/Button'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='flex-col gap-4  p-5' >
+      
+      <Button variant='ghost' startIcon={<Plus size={12}/>} text='Hello World' size='sm'/>
+      <Button variant='ghost' startIcon={<Plus size={14}/>} text='Hello World' size='md'/>
+      <Button variant='ghost' startIcon={<Plus size={16}/>} text='Hello World' size='lg'/>
+    </div>
   )
 }
 
