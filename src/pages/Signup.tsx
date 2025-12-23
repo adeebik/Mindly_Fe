e5df324e7d93125b441f7f4ad4afe1a3ef8b1b2a
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-24 02:26:17 +0530

    connecting signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,21 +1,42 @@
+import { useRef } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import Auth from "../Layout/Auth";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 
 export default function Signup() {
+  const nameRef = useRef<HTMLInputElement>();
+  const userRef = useRef<HTMLInputElement>();
+  const passRef = useRef<HTMLInputElement>();
+
+  async function signup (){
+    const username = nameRef.current.value
+    const usermail = userRef.current.value;
+    const password = passRef.current.value;
+
+    await axios.post(`${BACKEND_URL + "/users/signup"}`, {
+      name: username,
+      email: usermail,
+      password: password
+    } )
+    alert("Signed Up!")
+  }
+
   return (
      <Auth title="Create Account" subtitle="Start organizing your content" redirectLink="" redirectText="Sign in" redirectTitle="Already have an account?">
           <>
-            <Input label="Name" placeholder="John Doe" type="text" />
-            <Input label="Email" placeholder="you@example.com" type="mail" />
-            <Input label="Password" placeholder="*******" type="password" />
+            <Input reference={nameRef} label="Name" placeholder="John Doe" type="text" />
+            <Input reference={userRef} label="Email" placeholder="you@example.com" type="mail" />
+            <Input reference={passRef} label="Password" placeholder="*******" type="password" />
             <Button
               variant="primary"
               size="md"
               text="Create Account"
               fullWidth={true}
               type="submit"
+              onclick={signup}
             />
           </>
         </Auth>
