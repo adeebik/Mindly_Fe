b414d081d415e6f60f324e4b339f4f014001214b
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-23 21:02:50 +0530

    ui fixes

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 w-screen h-14 bg-white border-b border-zinc-200 p-3">
       <div className="main flex justify-between items-center">
-        <div className="logo flex items-center gap-2">
+        <div className="logo flex items-center gap-2 ml-3">
           <p className="text-white bg-blue-600 rounded-lg px-2.5 py-1 font-bold"> M </p>
           <p className="font-bold text-zinc-800">Mindly</p>
         </div>
-        <div className="buttons flex items-center gap-3">
+        <div className="buttons flex items-center gap-3 mr-6">
           <Button size="sm" variant="secondary" startIcon={<Globe size={16}/>} text="Share Brain" />
           <p className="text-sm font-normal text-zinc-700 mx-2">Adeeb</p>
           <Button size="sm" variant="ghost" startIcon={<LogOut size={16}/>} text="Logout" />
