297f546560b012e928c2d53e28293c24b54245e9
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-25 00:15:33 +0530

    twitter embed fixed

--- src/components/TwitterEmbed.tsx
+++ src/components/TwitterEmbed.tsx
@@ -1,24 +1,37 @@
-export default function TwitterEmbed() {
+import { ExternalLink, Twitter } from "lucide-react";
+import { Tweet } from "react-tweet";
+
+export default function TwitterEmbed({ url }: { url: string }) {
+  const getTweetId = (url: string) => {
+    const match = url.match(/status\/(\d+)/);
+    return match ? match[1] : null;
+  };
+
+  const tweetId = getTweetId(url);
+  if (!tweetId) {
+    return (
+      <div className="bg-gray-50 rounded-lg text-center">
+        <p className="text-gray-500 text-xs">Invalid Twitter URL</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="tweet">
-      <blockquote className="twitter-tweet">
-        <p lang="en" dir="ltr">
-          <a href="https://twitter.com/kirat_tw?ref_src=twsrc%5Etfw">
-            @kirat_tw
-          </a>{" "}
-          Dropped another bomb.<br></br> Excited to join and Explore{" "}
-          <a href="https://t.co/Fw0vIit1iY">pic.twitter.com/Fw0vIit1iY</a>
-        </p>
-        &mdash; Ayush Gopal (@iamAyushgopal){" "}
-        <a href="https://twitter.com/iamAyushgopal/status/2002742332660257012?ref_src=twsrc%5Etfw">
-          December 21, 2025
+    <div className="bg-gray-50 rounded-lg px-3 pb-3 border border-gray-200">
+        <div className="tweet">
+          <Tweet id={tweetId} />
+
+      </div>
+      <div className="bg-white rounded-lg p-3 border border-gray-200">
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:text-blue-700 text-xs flex items-center gap-1 font-medium"
+        >
+          View on Twitter <ExternalLink size={12} />
         </a>
-      </blockquote>{" "}
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charSet="utf-8"
-      ></script>
+      </div>
     </div>
   );
 }
